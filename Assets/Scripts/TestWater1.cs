66402ecc39bc16200163a10d607cba8fc771121d
LukeN25 <C22532639@mytudublin.ie>
2024-02-09 08:57:01 +0000

    weird water testing

---
+++ Assets/Scripts/TestWater1.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class TestWater1 : MonoBehaviour
+{
+    public float scale1;
+    public float waveSpeed1;
+    public float waveHeight1;
+    void Update()
+    {
+        CalcNoise1();
+    }
+    private void CalcNoise1()
+    {
+        // Get a reference to the Mesh Filter
+        MeshFilter mF = GetComponent<MeshFilter>();
+        // Create array from Mesh vertices
+        Vector3[] verts = mF.mesh.vertices;
+        // Iterate through vertices, multiply by scale value and add wavespeed
+        //
+        for (int i = 0; i < verts.Length; i++)
+        {
+            float pX = (verts[i].x * scale1) + (Time.time * waveSpeed1) * -2;
+            float pZ = (verts[i].z * scale1) + (Time.time * waveSpeed1) * -2;
+            // Update the Y component of each vertices using perlin noise
+            verts[i].y = Mathf.PerlinNoise(pX, pZ) * waveHeight1;
+        }
+        // Update the vertices
+        mF.mesh.vertices = verts;
+        // Recalculate normals as triangles have changed
+        mF.mesh.RecalculateNormals();
+        // Recalculate bounds as triangles have changed
+        mF.mesh.RecalculateBounds();
+    }
+}
+
+
+
