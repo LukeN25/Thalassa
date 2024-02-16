310cf5f7fb213cc4f348bb784530f9091cf231b8
Evan100354 <evanlong100354@gmail.com>
2024-02-16 17:07:10 +0000

    Pipe Manager working perfectly and raycast layer detectors added

--- Assets/PipeMinigame/PipeManager.cs
+++ Assets/PipeMinigame/PipeManager.cs
@@ -4,33 +4,49 @@ using UnityEngine;
 
 public class PipeManager : MonoBehaviour
 {
-    public Transform Pipe1;
-    public Transform Pipe2;
-    public Transform Pipe3;
-    public Transform Pipe4;
-    public Transform Pipe5;
-    public Transform Pipe6;
+    public GameObject Pipe1;
+    public GameObject Pipe2;
+    public GameObject Pipe3;
+    public GameObject Pipe4;
+    public GameObject Pipe5;
+    public GameObject Pipe6;
 
+    public LayerMask pipeRight;
 
+    Vector3 _direction1;
+    Vector3 _direction2;
+    Vector3 _direction3;
+    Vector3 _direction4;
+    Vector3 _direction5;
+    Vector3 _direction6;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool pipe1Right;
+    public bool pipe2Right;
+    public bool pipe3Right;
+    public bool pipe4Right;
+    public bool pipe5Right;
+    public bool pipe6Right;
 
-    // Update is called once per frame
+    
     void Update()
     {
-        //if(Pipe1.transform.rotation.x == -180 && Pipe2.transform.rotation.x == -180 && Pipe3.transform.rotation.x == -180 && 
-            //Pipe4.transform.rotation.x == -180 && Pipe5.transform.rotation.x == -180 && Pipe6.transform.rotation.x == -180)
-        {
-            //Debug.Log("Won");
-        }
+        _direction1 = Pipe1.transform.up * -1;
+        _direction2 = Pipe2.transform.up * -1;
+        _direction3 = Pipe3.transform.up * -1;
+        _direction4 = Pipe4.transform.up * -1;
+        _direction5 = Pipe5.transform.up * -1;
+        _direction6 = Pipe6.transform.up * -1;
 
-        if (Pipe1.eulerAngles.x == -180)
+        pipe1Right = Physics.Raycast(Pipe1.transform.position, _direction1, 10f , pipeRight);
+        pipe2Right = Physics.Raycast(Pipe2.transform.position, _direction1, 10f , pipeRight);
+        pipe3Right = Physics.Raycast(Pipe3.transform.position, _direction1, 10f , pipeRight);
+        pipe4Right = Physics.Raycast(Pipe4.transform.position, _direction1, 10f , pipeRight);
+        pipe5Right = Physics.Raycast(Pipe5.transform.position, _direction1, 10f , pipeRight);
+        pipe6Right = Physics.Raycast(Pipe6.transform.position, _direction1, 10f , pipeRight);
+
+        if(pipe1Right == true && pipe2Right == true && pipe3Right == true && pipe4Right == true && pipe5Right == true && pipe6Right == true)
         {
-            Debug.Log("Won");
+            Debug.Log("Win");
         }
     }
 }
